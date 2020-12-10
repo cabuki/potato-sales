b443e049357ac867b944c1221ea4e83d57e0c3df
Jon McLennan <cabuki@gmail.com>
2020-12-10 11:57:54 +0100

    Added basic sales page and controller. (Does not contact API yet.)

---
+++ src/Controller/SalesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class SalesController extends AbstractController
+{
+    public function sales(): Response
+    {
+        $product = new Product();
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        return $this->render(
+            'sales.html.twig',
+            [
+                'pagename' => 'Sales'
+            ]
+        );
+    }
+}
